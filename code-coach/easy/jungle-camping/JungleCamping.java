e82feafc552db1031a178a0938788041c9caf6c0
muhrafitriandi <61121146+muhrafitriandi@users.noreply.github.com>
2021-06-28 23:21:08 +0700

    Update JungleCamping.java

--- code-coach/easy/jungle-camping/JungleCamping.java
+++ code-coach/easy/jungle-camping/JungleCamping.java
@@ -6,9 +6,8 @@ public class JungleCamping {
 		String noises = scanner.nextLine();
 		String[] splitNoises = noises.split(" ");
 
-		String animals = "";
 		for (int i=0; i<splitNoises.length; i++) {
-			animals = splitNoises[i].equals("Grr") ? "Lion": splitNoises[i].equals("Rawr") ? "Tiger": splitNoises[i].equals("Ssss") ? "Snake": "Bird";
+			String animals = splitNoises[i].equals("Grr") ? "Lion": splitNoises[i].equals("Rawr") ? "Tiger": splitNoises[i].equals("Ssss") ? "Snake": "Bird";
 			if (i != 0) 
 				System.out.print(" ");
 
