9fa027f126dc94c1488f49703ed795de0a04eb83
muhrafitriandi <61121146+muhrafitriandi@users.noreply.github.com>
2021-07-01 22:25:09 +0700

    Update TheSpyLife.java

--- code-coach/medium/the-spy-life/TheSpyLife.java
+++ code-coach/medium/the-spy-life/TheSpyLife.java
@@ -11,9 +11,9 @@ public class TheSpyLife {
 		String temp = "";
 		for (int i = encodedMessage.length()-1; i >= 0; i--) {
 			if (encodedMessage.charAt(i) >= 'A' && encodedMessage.charAt(i) <= 'Z' ||
-				encodedMessage.charAt(i) >= 'a' && encodedMessage.charAt(i) <= 'z' ||
-				encodedMessage.charAt(i) == ' ') {
-				temp += encodedMessage.charAt(i);	
+			    encodedMessage.charAt(i) >= 'a' && encodedMessage.charAt(i) <= 'z' ||
+			    encodedMessage.charAt(i) == ' ') {
+			    temp += encodedMessage.charAt(i);	
 			}
 		}
 		return temp;
