55a63746836e8f15abb40e0922a69740ed323ce7
muhrafitriandi <61121146+muhrafitriandi@users.noreply.github.com>
2021-07-01 22:23:20 +0700

    Add files via upload

---
+++ code-coach/medium/the-spy-life/TheSpyLife.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class TheSpyLife {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		String encodedMessage = scanner.nextLine();
+		System.out.print(isLetter(encodedMessage));
+	}
+
+	public static String isLetter(String encodedMessage) {
+		String temp = "";
+		for (int i = encodedMessage.length()-1; i >= 0; i--) {
+			if (encodedMessage.charAt(i) >= 'A' && encodedMessage.charAt(i) <= 'Z' ||
+				encodedMessage.charAt(i) >= 'a' && encodedMessage.charAt(i) <= 'z' ||
+				encodedMessage.charAt(i) == ' ') {
+				temp += encodedMessage.charAt(i);	
+			}
+		}
+		return temp;
+	}
+}
